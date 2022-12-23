9e2af5c91cff5b28d8018064ab57bb023a863a54
Rui Silva <ruisinhofilipe@gmail.com>
2022-12-23 18:26:30 +0100

    Few changes on the Display code and added Game winner function for later

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const boardCells = document.querySelectorAll('.boardCell');
+let array = ['', '', '', '', '', '', '', '', ''];
+let index;
 
 const playerFactory = (mark, turn) => {
     return { mark, turn }
@@ -7,21 +8,67 @@ const playerFactory = (mark, turn) => {
 const player1 = playerFactory('X', true);
 const player2 = playerFactory('O', false);
 
+//Display marks of each player
+const boardCells = document.querySelectorAll('.boardCell');
 boardCells.forEach(cells => {
     cells.addEventListener('click', () => {
         if (cells.textContent === '') {
             if (player1.turn === true) {
+                index = cells.getAttribute('data-index');
                 cells.textContent = player1.mark;
+                array.splice(index, 1, player1.mark);
                 player1.turn = false;
                 player2.turn = true;
+                gameWinner()
             } else if (player2.turn === true) {
+                index = cells.getAttribute('data-index');
+                cells.textContent = player2.mark;
+                array.splice(index, 1, player2.mark);
                 cells.textContent = player2.mark;
                 player2.turn = false;
                 player1.turn = true;
+                gameWinner()
             }
         }
-    })
+    });
 });
+
+// Game winner
+function gameWinner() {
+    if (
+        (array[0] == 'X' && array[1] == 'X' && array[2] == 'X') ||
+        (array[0] == 'X' && array[3] == 'X' && array[6] == 'X') ||
+        (array[0] == 'X' && array[4] == 'X' && array[8] == 'X') ||
+        (array[1] == 'X' && array[4] == 'X' && array[5] == 'X') ||
+        (array[2] == 'X' && array[5] == 'X' && array[8] == 'X') ||
+        (array[2] == 'X' && array[4] == 'X' && array[6] == 'X') ||
+        (array[3] == 'X' && array[4] == 'X' && array[5] == 'X') ||
+        (array[6] == 'X' && array[7] == 'X' && array[8] == 'X')
+    ) {
+        console.log('X wins');
+    } else if (
+        (array[0] == 'O' && array[1] == 'O' && array[2] == 'O') ||
+        (array[0] == 'O' && array[3] == 'O' && array[6] == 'O') ||
+        (array[0] == 'O' && array[4] == 'O' && array[8] == 'O') ||
+        (array[1] == 'O' && array[4] == 'O' && array[5] == 'O') ||
+        (array[2] == 'O' && array[5] == 'O' && array[8] == 'O') ||
+        (array[2] == 'O' && array[4] == 'O' && array[6] == 'O') ||
+        (array[3] == 'O' && array[4] == 'O' && array[5] == 'O') ||
+        (array[6] == 'O' && array[7] == 'O' && array[8] == 'O')
+    ) {
+        console.log('O wins')
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
