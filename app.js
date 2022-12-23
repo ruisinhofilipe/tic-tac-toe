0dba11766233d1c17e9eee4a4ef90b4ca0f0bd4c
Rui Silva <ruisinhofilipe@gmail.com>
2022-12-23 15:00:16 +0100

    few mods

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const boardCells = document.querySelectorAll('.boardCell');
+let index = '';
+let game = ['', '', '', '', '', '', '', '', ''];
 
-boardCells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        if (cell.textContent === 'x') {
-            cell.textContent = 'o';
-        } else {
-            cell.textContent = 'x';
-        }
-    })
-});
+
+function playGame() {
+
+    boardCells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            if (cell.textContent === '' || cell.textContent === 'O') {
+                cell.textContent = 'X';
+                index = cell.getAttribute('data-index');
+                game[index] = 'X';
+                console.log(game[0]);
+            } else {
+                cell.textContent = 'O';
+                index = cell.getAttribute('data-index');
+                game[index] = 'O';
+                console.log(game[0]);
+            }
+        })
+    });
+}
+
+playGame();
+
+// && game[4] !== 'X' && game[8] !== 'X'
