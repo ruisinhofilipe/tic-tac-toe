918c8c7756e6946817df73339760cf440e8a974d
Rui Silva <ruisinhofilipe@gmail.com>
2022-12-28 07:55:38 +0100

    Last few mods on the code, correctly display each player turns

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let array = ['', '', '', '', '', '', '', '', ''];
 let index;
-let gameResult;
+
 
 // Get player names, mark and set their turn 
 const playerFactory = (name, mark, turn) => {
@@ -14,57 +14,35 @@ const playerFactory = (name, mark, turn) => {
     return { getName, mark, turn }
 }
 
-const player1 = playerFactory('player1', 'X', true);
-const player2 = playerFactory('player2', 'O', false);
+const player1 = playerFactory('Player1', 'X', true);
+const player2 = playerFactory('Player2', 'O', false);
 
 
 // Display player names on the page
 function displayNames() {
-    const p1Name = document.querySelector('.playerOne');
-    const p2Name = document.querySelector('.playerTwo');
+    const playerTurn = document.querySelector('.playerTurn');
 
     //Player 1 name
-    p1Name.textContent = `${player1.getName()} using ${player1.mark}`;
-
-    // Player 2 name
-    p2Name.textContent = `${player2.getName()} using ${player2.mark}`;
-
+    if (player1.turn === true) {
+        playerTurn.textContent = `${player1.getName()}\'s turn - ${player1.mark}`;
+    } else {
+        playerTurn.textContent = `${player2.getName()}\'s turn - ${player2.mark}`;
+    }
 }
 
 //Display marks of each player
 const startGameButton = document.querySelector('.startGame');
 startGameButton.addEventListener('click', () => {
 
-    // Displaying players names
+    // Hide players input boxes 
+    hiddenPlayerInformation();
+    // Display which player goes first
     displayNames();
+    // Start Game 
+    startGame();
+});
 
-    //Display plays of each player
-    const boardCells = document.querySelectorAll('.boardCell');
-    boardCells.forEach(cells => {
-        cells.addEventListener('click', () => {
-            if (cells.textContent === '') {
-                if (player1.turn === true) {
-                    index = cells.getAttribute('data-index');
-                    cells.textContent = player1.mark;
-                    array.splice(index, 1, player1.mark);
-                    player1.turn = false;
-                    player2.turn = true;
-                    possibleOutcomes();
-                } else if (player2.turn === true) {
-                    index = cells.getAttribute('data-index');
-                    cells.textContent = player2.mark;
-                    array.splice(index, 1, player2.mark);
-                    cells.textContent = player2.mark;
-                    player2.turn = false;
-                    player1.turn = true;
-                    possibleOutcomes();
-                }
-            }
-        });
-    });
-})
-
-// Possible outcomes of win
+// Possible win outcomes for each player / draw included
 function possibleOutcomes() {
 
     // Player 1 possible win scenarios
@@ -79,6 +57,7 @@ function possibleOutcomes() {
         (array[6] == 'X' && array[7] == 'X' && array[8] == 'X')
     ) {
         winner(player1.getName());
+        displayFinalResult();
     }
     // Player 2 possible win scenarios
     else if (
@@ -92,18 +71,71 @@ function possibleOutcomes() {
         (array[6] == 'O' && array[7] == 'O' && array[8] == 'O')
     ) {
         winner(player2.getName());
+        displayFinalResult();
     }
     // In case of a draw
     else if (array.every(drawGame)) {
         winner('That\'s a draw! No one');
+        displayFinalResult();
     }
 }
+
+
+function startGame() {
+    //Display plays of each player
+    const boardCells = document.querySelectorAll('.boardCell');
+    boardCells.forEach(cells => {
+        cells.addEventListener('click', () => {
+            if (cells.textContent === '') {
+                if (player1.turn === true) {
+                    index = cells.getAttribute('data-index');
+                    cells.textContent = player1.mark;
+                    array.splice(index, 1, player1.mark);
+                    player1.turn = false;
+                    player2.turn = true;
+                    displayNames();
+                    possibleOutcomes();
+                } else if (player2.turn === true) {
+                    index = cells.getAttribute('data-index');
+                    cells.textContent = player2.mark;
+                    array.splice(index, 1, player2.mark);
+                    cells.textContent = player2.mark;
+                    player2.turn = false;
+                    player1.turn = true;
+                    displayNames();
+                    possibleOutcomes();
+                }
+            }
+        });
+    });
+}
+
 
 function drawGame(mark) {
     return mark === 'X' || mark === 'O';
 }
 
 function winner(winner) {
-    document.querySelector('.outcomeGame').textContent = `${winner} has won!`;
+    document.querySelector('.finalResult').textContent = `${winner} has won!`;
 }
+
+function hiddenPlayerInformation() {
+    // Hide player names input box
+    let playerBoxInput = document.querySelector('.startGameInformations');
+    playerBoxInput.classList.add('hidden');
+
+    // Remove hidden and display player's turn
+    let playerTurn = document.querySelector('.playerTurn');
+    playerTurn.classList.add('remove-hidden');
+}
+
+function displayFinalResult() {
+    let finalResultDisplay = document.querySelector('.finalResult');
+    finalResultDisplay.classList.add('remove-hidden');
+
+    document.querySelector('.playerTurn').classList.remove('remove-hidden')
+}
+
+
+
 
